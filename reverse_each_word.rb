1ad9c369bfb6d12a21a0c66251cd69ef0a696e5e
dynamic-daemon-637b <zl3087@columbia.edu>
2021-06-03 22:05:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
 def reverse_each_word(sentence)
-  array= sentence.split("")
+  array= sentence.split(" ")
   reverse=[]
   array.collect { |word| 
 
   reverse << word.reverse}
-reverse.join("")
+reverse.join(" ")
 end
 
 
